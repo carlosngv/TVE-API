e99f41af895d44d815bdca09f241a8aeed572929
carlosngv <carlosngv99@hotmail.com>
2021-02-21 16:00:11 -0600

    Arreglado para prod

--- index.js
+++ index.js
@@ -2,25 +2,32 @@ const cors = require('cors');
 const express = require('express');
 require('dotenv').config();
 const { dbConnection } = require('./db/config');
+const path = require('path');
 const { router } = require('./routes/routes');
-const {routerTest} = require('./routes/routesMux.js');
-const {routerQueries} = require('./routes/queries');
+const { routerTest } = require('./routes/routesMux.js');
+const { routerQueries } = require('./routes/queries');
 const app = express();
 
 // DB Connection
 dbConnection();
 
+app.use(express.static('public'));
 // settings
 app.set('port', process.env.PORT || 3000);
-
 // middlawares
 app.use(cors());
 app.use(express.json());
 
 
 app.use('/api/v1/auth', router);
-app.use('/',routerTest);
-app.use('/logic' , routerQueries);
+app.use('/', routerTest);
+app.use('/logic', routerQueries);
+
+// Manejador de rutas
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+
 
 app.listen(app.get('port'), () => {
     console.log(`Server running on port ${app.get('port')}`);
